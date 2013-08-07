4df1746c51202ef789f6802e5aacd236e1801740
Lim Sungguk <limasdf@gmail.com>
2013-08-07 14:37:35 +0900

    Update base.php

--- OHTV/base.php
+++ OHTV/base.php
@@ -4,12 +4,7 @@ function sendContentType() {
   header('Pragma: no-cache');
   header('Cache-Control: no-cache');
   header('Content-Style-Type: text/css');
-  $uagent = strtolower($_SERVER['HTTP_USER_AGENT']);
-  if (strstr($uagent, 'firefox') || strstr($uagent, 'chrome')) {
-     header('Content-Type: text/html; charset=UTF-8');
-  } else {
-    header('Content-Type: application/vnd.hbbtv.xhtml+xml; charset=UTF-8');
-  }
+  header('Content-Type: application/vnd.ohtv.xhtml+xml; charset=UTF-8');
 }
 
 function videoObject($left=0, $top=0, $width=1280, $height=720) {
@@ -19,11 +14,10 @@ function videoObject($left=0, $top=0, $width=1280, $height=720) {
 function openDocument($title='OHTV testsuite', $allscripts=1, $addheaders='') {
   global $ROOTDIR;
   echo '<?xml version="1.0" encoding="utf-8" ?>'."\n";
-  echo '<!DOCTYPE html PUBLIC "-//HbbTV//1.1.1//EN" "http://www.hbbtv.org/dtd/HbbTV-1.1.1.dtd">'."\n";
   echo "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n";
   echo "<head>\n";
   echo "<title>$title</title>\n".$addheaders;
-  echo "<meta http-equiv=\"content-type\" content=\"Content-Type: application/vnd.hbbtv.xhtml+xml; charset=UTF-8\" />\n";
+  echo "<meta http-equiv=\"content-type\" content=\"Content-Type: application/vnd.ohtv.xhtml+xml; charset=UTF-8\" />\n";
   echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"$ROOTDIR/base.css\" />\n";
   echo "<script type=\"text/javascript\" src=\"$ROOTDIR/settings.js\"></script>\n";
   echo "<script type=\"text/javascript\" src=\"$ROOTDIR/releaseinfo.js\"></script>\n";
