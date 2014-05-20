646ad5c1b01b8026cfaaa48cefd76b3d0c7e24a4
Sungguk Lim <limasdf@gmail.com>
2014-05-20 09:11:50 -0400

    update2

--- OHTV/base.js
+++ OHTV/base.js
@@ -55,13 +55,18 @@ function menuInit() {
 }
 
 // TODO(sunglim): Rename variable name. Perhaps, index?.
-function menuSelect(i) {
+function menuSelect(index) {
   // TODO(sunglim): ASSET(i >= 0 || opts.length < opts.length);
-  selected = i;
+  if (index <=0) {
+    index = 0;
+  } else if (index>=opts.length) {
+    index = opts.length-1;
+  }
+  selected = index;
   var scroll = Math.max(0, Math.min(opts.length-13, selected-6));
-  for (i=0; i<opts.length; i++) {
-    opts[i].style.display = (i>=scroll && i<scroll+13) ? 'block' : 'none';
-    opts[i].className = selected==i ? 'lisel' : '';
+  for (index=0; index<opts.length; index++) {
+    opts[index].style.display = (index>=scroll && index<scroll+13) ? 'block' : 'none';
+    opts[index].className = selected==index ? 'lisel' : '';
   }
 }
 
